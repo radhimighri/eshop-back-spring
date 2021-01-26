d5425e2dfd3fc8900ae2a930bde2da06fd812402
Radhi Mighri <radhi.mighri@polytechncien.tn>
2021-01-26 15:09:05 +0100

    Final version

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    //query methods:
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
-    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
 }
